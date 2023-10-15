5d59a7c9a2eaa4c1efe828921bb8af2cd1594e9a
Kasper Nyhus <kanyhus@gmail.com>
2023-10-15 20:51:49 +0200

    update example

--- examples/main/main.c
+++ examples/main/main.c
@@ -14,6 +14,7 @@
 #include "esp_err.h"
 #include "esp_log.h"
 #include "esp_neopixel.h"
+#include "hal/gpio_types.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -28,11 +29,22 @@ void app_main(void)
 
     esp_neopixel_config_t cfg = {
         .rmt_channel = 0,
-        .gpio = 15
+        .gpio = GPIO_NUM_15
     };
     esp_neopixel_init(&cfg);
 
     while (1) {
+        ESP_LOGI(TAG, "Set pixel");
+        esp_neopixel_set_pixel(13, 30, 1);
+        esp_neopixel_on();
+        vTaskDelay(pdMS_TO_TICKS(1000));
+
+        ESP_LOGI(TAG, "OFF");
+        esp_neopixel_off();
+        vTaskDelay(pdMS_TO_TICKS(1000));
+
+        ESP_LOGI(TAG, "Set color");
+        esp_neopixel_set_color(NEOPIXEL_ORANGE);
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
